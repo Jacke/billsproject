0ae8406288976ccd073d063ec8303423e409e3e3
Stanislav <Sobolev>
2013-08-13 11:28:34 +0600

    Histories sum

--- app/helpers/histories_helper.rb
+++ app/helpers/histories_helper.rb
@@ -1,2 +1,12 @@
 module HistoriesHelper
+  mattr_reader :shifts_sum
+
+  def set_sum(type, vls)
+    @@shifts_sum = {} unless @@shifts_sum.class == Hash 
+    if @@shifts_sum[type].present?
+      @@shifts_sum[type] =+ vls
+    else
+      @@shifts_sum[type] = vls
+    end
+  end
 end
