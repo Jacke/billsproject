bcc05cd4e4e58cba40fb2961fb39f093a3856840
Jacke <iamjacke@gmail.com>
2013-08-14 10:22:44 +0400

    Ending almost

--- app/helpers/histories_helper.rb
+++ app/helpers/histories_helper.rb
@@ -10,11 +10,18 @@ module HistoriesHelper
     @diffs_sum
   end
 
-  Ð def set_ndi(vls)
-    #
+  def set_ndi(vls)
+    @native_diff = 0 if @native_diff.nil?
+    @native_diff += vls
   end
   def set_diffsum(type, vls)
-    #
+    @diffs_sum = {} if @diffs_sum.nil?
+    diffsum = @diffs_sum
+    if diffsum[type].present? && vls.present?
+      diffsum[type] += vls
+    else
+      diffsum[type] = vls if vls.present?
+    end
   end
 
   def set_sum(type, vls)
